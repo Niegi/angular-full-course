a9ab8fb44026eb24f5b8204fc997986548f619f6
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2019-10-04 18:08:28 +0200

    assignment_1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Server1Component } from './server1/server1.component';
+import { Server2Component } from './server2/server2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Server1Component,
+    Server2Component
   ],
   imports: [
     BrowserModule
